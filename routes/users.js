7f707e6e41ff71adf6123d23271bec142a4f4101
Sofilus <sofia00olsson@gmail.com>
2023-03-09 23:12:35 +0100

    Created a list of usernames and passwords

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+let users = [{username: "sofia", password: "sofia"},{username: "gustav", password: "gustav"} ];
 
 module.exports = router;
